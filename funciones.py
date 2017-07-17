caf67c5d189c3aad3e4d569e1232f86d85cf15ab
Saul11235 <edwinsaulpm@gmail.com>
2017-07-17 14:39:50 -0500

    se añade funciones.py

---
+++ funciones.py
@@ -0,0 +1,11 @@
+#este archivo declara funciones en el código
+
+def maniqui(fase):
+	'''fase es un nro que identifica el estado en el que esta el archivo, 0 inicia 5 pierde'''
+	if fase==0:
+		return '''
+
+		O
+		l
+
+		'''
